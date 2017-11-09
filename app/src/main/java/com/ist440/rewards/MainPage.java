cffd90d3ac03d3a5cf09c5445c344873c8cedb32
tmcbride73 <tmcbride7373@gmail.com>
2017-11-09 15:48:45 -0500

    Changed Names

--- app/src/main/java/com/ist440/rewards/MainPage.java
+++ app/src/main/java/com/ist440/rewards/MainPage.java
@@ -22,7 +22,7 @@ public class MainPage extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add your receipt now!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
