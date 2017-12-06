ca88bdad9ade3dbeefcf20c539c567dbdb307331
tmcbride73 <tmcbride7373@gmail.com>
2017-12-06 17:38:53 -0500

    Added Settings menu and maps link

--- app/src/main/java/com/ist440/rewards/MainPage.java
+++ app/src/main/java/com/ist440/rewards/MainPage.java
@@ -103,6 +103,8 @@ public class MainPage extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
